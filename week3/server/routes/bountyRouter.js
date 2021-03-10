e68888fa7d76f8d1ca589e23d4fae34c415a3ef1
MariMorse <marianne.morse@bryanuniversity.edu>
2021-03-10 13:35:18 -0500

    commit

---
+++ week3/server/routes/bountyRouter.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const bountyRouter = express.Router()
+const uuid = require("uuid/v4")
+
+applicationCache.us(express.json())
+
+let hunter = {
+    firstName: "Lord Darth",
+    lastName: "Sion",
+    living:  yes,
+    bountyAmount: 35,
+    type: "Sith",
+     
+}
+
+
+applicationCache.use("/", require("./routes/bountyRouter.js"))
+
+
+
+bountyRouter.get("/", req, res => {
+    res.send(bounties)
+})
+
+bountyRouter.post("/", (req, res) =>{
+    const newHunter = req.body
+    newHunter._id = uuid()
+    hunter.push(newHunter)
+
+})
+
+
+
+
+
+
+
+module.exports = bountyRouter
---
+++ week3/server/server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(9000, () => {
+    console.log("express router")
+})
