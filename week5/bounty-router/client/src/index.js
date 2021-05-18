f952ecf9d24a95d9c17cd7e43391e23432b7b7c4
MariMorse <marianne.morse@bryanuniversity.edu>
2021-05-17 21:49:23 -0400

    commit

--- week5/bounty-router/client/src/index.js
+++ week5/bounty-router/client/src/index.js
@@ -1,1 +1,14 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default index
 
