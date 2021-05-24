8d32495379375e27b72ec393c454098e02b515fe
MariMorse <marianne.morse@bryanuniversity.edu>
2021-05-24 00:43:15 -0400

    commit

--- week5/bounty-router/server/server.js
+++ week5/bounty-router/server/server.js
@@ -2,4 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+//Middleware 
 app.use('/', express.json())
+app.use(morgan('dev'))
+
+// Routes
+app.use('/bounties', require('./routes/bountyRouter'))
+
+//Server Listen
+app.listen(9000, () => {
+    console.log('The server is listening on port 9000.');
+});
