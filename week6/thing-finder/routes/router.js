de6c258e53cf6c25e37cb39a28f37f4799e98e24
MariMorse <marianne.morse@bryanuniversity.edu>
2021-05-27 00:14:52 -0400

    commit

--- week6/thing-finder/routes/router.js
+++ week6/thing-finder/routes/router.js
@@ -6,37 +6,37 @@ const plants = [
         name: 'rose',
         type: 'flower',
         color: 'yellow',
-        amount: 12.99
+        amount: 12.99,
     },
     {
         name: 'lavender',
         type: 'herb',
         color: 'violet',
-        amount: 4.75
+        amount: 4.75,
     },
     {
         name: 'daisies',
         type: 'flower',
         color: 'blue',
-        amount: 2.50
+        amount: 2.50,
     },
     {
         name: 'buttonbush',
         type: 'flower',
         color: 'white',
-        amount: 5.00
+        amount: 5.00,
     },
     {
         name: 'sage',
         type: 'herb',
         color: 'green',
-        amount: 3.15
+        amount: 3.15,
     },
     {
         name: 'garlic',
         type: 'herb',
         color: 'white',
-        amount: .75
+        amount: .75,
     }
 ]
 
