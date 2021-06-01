34479772a69217697f79882697c7bb12a8ebdbe6
MariMorse <marianne.morse@bryanuniversity.edu>
2021-05-31 20:16:08 -0400

    commit

--- week6/bounty_hunter/src/App.js
+++ week6/bounty_hunter/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Bounty from './components/Bounty'
+import AddBountyForm from './components/AddBountyForm'
 
 function App() {
+  const [bounties, setBounties] = useState([])
+
+  function getBounties() {
+      axios.get('/bounties')
+          .then(res => setBounties(res.data))
+          .catch(err => console.log(err))
+  }
+
+  function addBounty(newBounty) {
+      axios.post('/bounties', newBounty)
+          .then(res => {
+              setBounties(prevBounties => [...prevBounties, res.data])
+              console.log(res) 
+          })
+          .catch(err => console.log(err))
+  }
+
+  function deleteBounty(bountyId) {
+      axios.delete(`/bounties/${bountyId}`)
+          .then(res => {
+              setBounties(prevBounties => prevBounties.filter(bounty => bounty._id !== bountyId))
+              console.log(res) 
+          })
+          .catch(err => console.log(err))
+  }
+
+  function editBounty(updates, bountyId) {
+      axios.put(`/bounties/${bountyId}`, updates)
+          .then(res => {
+              setBounties(prevBounties => prevBounties.map(bounty => bounty._id !== bountyId ? bounty : res.data))
+
+              
+              console.log(res) 
+          })
+          .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+      getBounties()
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='bounty-container'>
+          <AddBountyForm
+              submit={addBounty}
+              btnText={'Add Bounty'}
+              toggle={() => { }} 
+          />
+          {
+              bounties.map((bounty, index) =>
+                  <Bounty
+                      {...bounty}
+                      key={index}
+                      deleteBounty={deleteBounty}
+                      editBounty={editBounty}/>)
+          }
+      </div>
+  )
 }
 
-export default App;
+export default App
