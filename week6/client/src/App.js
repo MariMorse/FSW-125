0b8faa5e399c8fa5fbde393b615b2fdd66ac208e
MariMorse <marianne.morse@bryanuniversity.edu>
2021-05-31 20:34:05 -0400

    commit

--- week6/client/src/App.js
+++ week6/client/src/App.js
@@ -1,69 +1,90 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Bounty from './components/Bounty'
-import AddBountyForm from './components/AddBountyForm'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Bounty from "./components/Bounty";
+import AddBountyForm from "./components/AddBountyForm";
 
 function App() {
-  const [bounties, setBounties] = useState([])
+  const [bounties, setBounties] = useState([]);
 
   function getBounties() {
-      axios.get('/bounties')
-          .then(res => setBounties(res.data))
-          .catch(err => console.log(err))
+    axios
+      .get("/bounties")
+      .then((res) => setBounties(res.data))
+      .catch((err) => console.log(err));
   }
 
   function addBounty(newBounty) {
-      axios.post('/bounties', newBounty)
-          .then(res => {
-              setBounties(prevBounties => [...prevBounties, res.data])
-              console.log(res) 
-          })
-          .catch(err => console.log(err))
+    axios
+      .post("/bounties", newBounty)
+      .then((res) => {
+        setBounties((prevBounties) => [...prevBounties, res.data]);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }
 
   function deleteBounty(bountyId) {
-      axios.delete(`/bounties/${bountyId}`)
-          .then(res => {
-              setBounties(prevBounties => prevBounties.filter(bounty => bounty._id !== bountyId))
-              console.log(res) 
-          })
-          .catch(err => console.log(err))
+    axios
+      .delete(`/bounties/${bountyId}`)
+      .then((res) => {
+        setBounties((prevBounties) =>
+          prevBounties.filter((bounty) => bounty._id !== bountyId)
+        );
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }
 
   function editBounty(updates, bountyId) {
-      axios.put(`/bounties/${bountyId}`, updates)
-          .then(res => {
-              setBounties(prevBounties => prevBounties.map(bounty => bounty._id !== bountyId ? bounty : res.data))
+    axios
+      .put(`/bounties/${bountyId}`, updates)
+      .then((res) => {
+        setBounties((prevBounties) =>
+          prevBounties.map((bounty) =>
+            bounty._id !== bountyId ? bounty : res.data
+          )
+        );
 
-              
-              console.log(res) 
-          })
-          .catch(err => console.log(err))
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
-      getBounties()
-  }, [])
-  
+    getBounties();
+  }, []);
+
   return (
-      <div className='bounty-container'>
-          <AddBountyForm
-              submit={addBounty}
-              btnText={'Add Bounty'}
-              toggle={() => { }} 
+    <div>
+      <div className="bounty-container">
+        <AddBountyForm
+          submit={addBounty}
+          btnText={"Add Bounty"}
+          toggle={() => {}}
+        />
+        {bounties.map((bounty, index) => (
+          <Bounty
+            {...bounty}
+            key={index}
+            deleteBounty={deleteBounty}
+            editBounty={editBounty}
           />
-          {
-              bounties.map((bounty, index) =>
-                  <Bounty
-                      {...bounty}
-                      key={index}
-                      deleteBounty={deleteBounty}
-                      editBounty={editBounty}/>)
-          }
+        ))}
       </div>
-  )
+
+      <div className="searchBox">
+        <Search />
+        <ul className="bountyFiltered">
+          {filteredBounty.map((bounty) => (
+            <li key={bounty._id}>
+              {bounty.firstName}
+              {bounty.lastName} -{bounty.type}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-
-
-export default App
+export default App;
