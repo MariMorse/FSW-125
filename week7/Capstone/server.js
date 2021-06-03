8e5956b73e5d2f986161291e8892e598d8f96fd7
MariMorse <marianne.morse@bryanuniversity.edu>
2021-06-02 23:05:38 -0400

    commit

--- week7/Capstone/server.js
+++ week7/Capstone/server.js
@@ -1,4 +1,12 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 const app = express()
 
+app.use('/', express.json());
+app.use(morgan('dev'));
+
+app.use('/spiders', require('./routes/router'));
+
+app.listen(5000, () => {
+    console.log('The server is listening on port 8000.');
+})
